4f309db9a09b3b8e37a19384fae0931e184bd07c
Echo_1-1 <fya17102007@gmail.com>
2025-10-08 15:49:06 +0500

    Tests

--- main.cpp
+++ main.cpp
@@ -6,8 +6,21 @@ int main() {
 		BinaryTree<int>* var1 = new BinaryTree<int>(1);
 		var1->NodeAssignAuto(new BinaryTree<int>(2));
 		std::cout << *var1->GetValue() << std::endl;
+
 		std::cout << "Left Node" << std::endl;
 		std::cout << *var1->GetLeftNode()->GetValue() << std::endl;
+
+		std::cout << "Let's try finding something" << std::endl;
+		BinaryTree<int>* var2Find = new BinaryTree<int>(-1);
+		var2Find = var1->GetNodeWithValue(new int(2));
+		std::cout << *var2Find->GetValue() << std::endl;
+
+
+		std::cout << "Now let's try finding something that does NOT exist, five, for example" << std::endl;
+
+		var2Find = var1->GetNodeWithValue(new int(5));
+		std::cout << *var2Find->GetValue() << std::endl;
+		std::cout << "Welp. It didn't found anything as i can see from 'ere" << std::endl;
 	}
 	catch (std::exception& ex){
 		std::cout << ex.what();
