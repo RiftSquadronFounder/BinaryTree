3d9d3fc2a06e8c6b8938aa142e768ff656a410ad
Echo_1-1 <fya17102007@gmail.com>
2025-10-08 15:07:47 +0500

    Template is not what i thought was right...

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -9,7 +9,6 @@ BinaryTree<T>::BinaryTree() {
 	_leftNode = nullptr;
 	_rightNode = nullptr;
 	_parentNode = nullptr;
-	_value = nullptr;
 }
 
 template <typename T>
@@ -17,7 +16,6 @@ BinaryTree<T>::BinaryTree(T* value) {
 	_leftNode = nullptr;
 	_rightNode = nullptr;
 	_parentNode = nullptr;
-	_value = value;
 }
 
 template <typename T>
@@ -25,8 +23,6 @@ BinaryTree<T>::BinaryTree(T value) {
 	_leftNode = nullptr;
 	_rightNode = nullptr;
 	_parentNode = nullptr;
-	_value = new T();
-	*_value = value;
 }
 
 
@@ -38,159 +34,3 @@ BinaryTree<T>::~BinaryTree() {
 }
 
 
-
-
-
-
-
-
-template <typename T>
-void BinaryTree<T>::SortWholeTree() {
-	BinaryTree<T>** NodeHolder = new BinaryTree<T>();
-
-	if (_rightNode != nullptr) {
-        _rightNode->SortWholeTree();
-    }
-    if (_leftNode != nullptr) {
-        _leftNode->SortWholeTree();
-    }
-}
-
-template <typename T>
-int BinaryTree<T>::CountNodesInTreeBeneath(BinaryTree<T>* NodeToCountFrom) {
-	if (NodeToCountFrom == nullptr) return 0;
-
-	int counter = 0;
-	if (NodeToCountFrom->GetLeftNode() != nullptr) {
-		counter += 1 + CountNodesInTreeBeneath(NodeToCountFrom->GetLeftNode());
-	}
-	if (NodeToCountFrom->GetRightNode() != nullptr) {
-		counter += 1 + CountNodesInTreeBeneath(NodeToCountFrom->GetRightNode());
-	}
-	return counter;
-}
-
-template <typename T>
-int BinaryTree<T>::GetWeight() {
-	int* counter = new int(0);
-	if (_rightNode != nullptr) {
-		counter++;
-	}
-	if (_leftNode != nullptr) {
-		counter++;
-	}
-	return counter;
-}
-
-
-template <typename T>
-void BinaryTree<T>::NodeAssignAuto(BinaryTree<T>* Node) {
-	if (_leftNode == nullptr) {
-		_leftNode = Node;
-		Node->_parentNode = this;
-	}
-	else if (_rightNode == nullptr) {
-		_rightNode = Node;
-		Node->_parentNode = this;
-	}
-	else {
-		if (Node->GetWeight() == 0 || Node->GetWeight() == 1) {
-			_rightNode->NodeAssignAuto(Node);
-		}
-		else {
-			_leftNode->NodeAssignAuto(Node);
-		}
-	}
-}
-
-
-
-
-
-template <typename T>
-void BinaryTree<T>::LeftNodeAssign(BinaryTree<T>* Node) {
-	*_leftNode = Node;
-}
-
-template <typename T>
-void BinaryTree<T>::RightNodeAssign(BinaryTree<T>* Node) {
-	*_rightNode = Node;
-}
-
-template <typename T>
-void BinaryTree<T>::ParentNodeAssign(BinaryTree<T>* Node) {
-	*_parentNode = Node;
-}
-
-
-
-template <typename T>
-void BinaryTree<T>::LeftNodeDisconnect() {
-	if (_leftNode != nullptr) {
-		_leftNode->_parentNode = nullptr;
-	}
-	_leftNode = nullptr;
-}
-
-template <typename T>
-void BinaryTree<T>::RightNodeDisconnect() {
-	if (_rightNode != nullptr) {
-		_rightNode->_parentNode = nullptr;
-	}
-	_rightNode = nullptr;
-}
-
-template <typename T>
-void BinaryTree<T>::ParentNodeDisconnect() {
-	_parentNode = nullptr;
-}
-
-
-
-
-
-template <typename T>
-T* BinaryTree<T>::GetValue() {
-	if (_value != nullptr) {
-		return _value;
-	}
-}
-
-
-template <typename T>
-BinaryTree<T>* BinaryTree<T>::GetNodeWithValue(T* value) {
-	if (_value != nullptr && *_value == *value) {
-		return this;
-	}
-
-	BinaryTree<T>* found = nullptr;
-	if (_leftNode != nullptr) {
-		found = _leftNode->GetNodeWithValue(value);
-		if (found != nullptr) return found;
-	}
-	if (_rightNode != nullptr) {
-		found = _rightNode->GetNodeWithValue(value);
-		if (found != nullptr) return found;
-	}
-	return nullptr;
-}
-
-
-
-
-template <typename T>
-BinaryTree<T>* BinaryTree<T>::GetLeftNode() {
-	return _leftNode;
-}
-
-template <typename T>
-BinaryTree<T>* BinaryTree<T>::GetRightNode() {
-	return _rightNode;
-
-}
-
-template <typename T>
-BinaryTree<T>* BinaryTree<T>::GetParentNode() {
-	return _parentNode;
-
-}
--- BinaryTree.hpp
+++ BinaryTree.hpp
@@ -11,35 +11,6 @@ public:
 	BinaryTree(T* value);
 	BinaryTree(T value);
 	~BinaryTree();
-
-
-	void NodeAssignAuto(BinaryTree<T>* Node);
-	void SortWholeTree();
-
-	int CountNodesInTreeBeneath(BinaryTree<T>* NodeToCountFrom);
-	int GetWeight();
-
-	void LeftNodeAssign(BinaryTree<T> *Node);
-	void RightNodeAssign(BinaryTree<T> *Node);
-	void ParentNodeAssign(BinaryTree<T> *Node);
-
-	void LeftNodeDisconnect();
-	void RightNodeDisconnect();
-	void ParentNodeDisconnect();
-
-	BinaryTree<T>* GetLeftNode();
-	BinaryTree<T>* GetRightNode();
-	BinaryTree<T>* GetParentNode();
-
-	T* GetValue();
-
-	BinaryTree<T>* GetNodeWithValue(T* value);
-
-
-
-
-
-
 private:
 	T* _value;
 	BinaryTree* _leftNode;
